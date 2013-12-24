86073b7830fd781cdb21026fe2244d1583b37877
wjiang <wjiang@codeaurora.org>
2013-12-24 14:27:52 +0800

    Music: Avoid repeat and shuffle icons don't display when rotating DUT.
    Add "android:configChanges" property in the AndroidManifest.xml so that
    every Activity life cycle will not call again when rotating the screen.
    Also add onConfigurationChanged() in MediaPlaybackActivity.java.
    
    Change-Id: Ie5c5547c218ba0d48201c89d52bcf25150f4b9a5
    Signed-off-by: Xiaojing Zhang <zhangx@codeaurora.org>

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -155,6 +155,55 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
 
         mTouchSlop = ViewConfiguration.get(this).getScaledTouchSlop();
     }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mAlbumArtWorker = new Worker("album art worker");
+        mAlbumArtHandler = new AlbumArtHandler(mAlbumArtWorker.getLooper());
+
+        setContentView(R.layout.audio_player);
+        mCurrentTime = (TextView) findViewById(R.id.currenttime);
+        mTotalTime = (TextView) findViewById(R.id.totaltime);
+        mProgress = (ProgressBar) findViewById(android.R.id.progress);
+        mAlbum = (ImageView) findViewById(R.id.album);
+        mArtistName = (TextView) findViewById(R.id.artistname);
+        mAlbumName = (TextView) findViewById(R.id.albumname);
+        mTrackName = (TextView) findViewById(R.id.trackname);
+
+        View v = (View)mArtistName.getParent();
+        v.setOnTouchListener(this);
+        v.setOnLongClickListener(this);
+
+        v = (View)mAlbumName.getParent();
+        v.setOnTouchListener(this);
+        v.setOnLongClickListener(this);
+
+        v = (View)mTrackName.getParent();
+        v.setOnTouchListener(this);
+        v.setOnLongClickListener(this);
+
+        mPrevButton = (RepeatingImageButton) findViewById(R.id.prev);
+        mPrevButton.setOnClickListener(mPrevListener);
+        mPrevButton.setRepeatListener(mRewListener, 260);
+        mPauseButton = (ImageButton) findViewById(R.id.pause);
+        mPauseButton.requestFocus();
+        mPauseButton.setOnClickListener(mPauseListener);
+        mNextButton = (RepeatingImageButton) findViewById(R.id.next);
+        mNextButton.setOnClickListener(mNextListener);
+        mNextButton.setRepeatListener(mFfwdListener, 260);
+        mQueueButton = (ImageButton) findViewById(R.id.curplaylist);
+        mQueueButton.setOnClickListener(mQueueListener);
+        mShuffleButton = ((ImageButton) findViewById(R.id.shuffle));
+        mShuffleButton.setOnClickListener(mShuffleListener);
+        mRepeatButton = ((ImageButton) findViewById(R.id.repeat));
+        mRepeatButton.setOnClickListener(mRepeatListener);
+
+        SeekBar seeker = (SeekBar) mProgress;
+        seeker.setOnSeekBarChangeListener(mSeekListener);
+        mProgress.setMax(1000);
+        mTouchSlop = ViewConfiguration.get(this).getScaledTouchSlop();
+        onStart();
+    }
     
     int mInitialX = -1;
     int mLastX = -1;
