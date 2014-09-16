8d9655eb309189640cfc249a1af7b53b70caf892
kaiyiz <kaiyiz@codeaurora.org>
2014-09-16 16:30:47 +0800

    Music: Reginal requirement for RTL
    Add flag for RTL support in manifest.
    Convert left/right layout properties to start/end properties.
    Adjust the visible positions for RTL layout.
    Mirror some 9-patch pictures to improve the RTL resolution.
    audio_player_common.xml is copied from res/layout/audio_player_common.xml.
    
    Change-Id: Ic40c7dd70ed67c62bfe2bff0b4f21c1ccf1ca224

--- src/com/android/music/TouchInterceptor.java
+++ src/com/android/music/TouchInterceptor.java
@@ -136,8 +136,25 @@ public class TouchInterceptor extends ListView {
                     mDragPointY = y - item.getTop();
                     mXOffset = ((int)ev.getRawX()) - x;
                     mYOffset = ((int)ev.getRawY()) - y;
-                    // The left side of the item is the grabber for dragging the item
-                    if (x < 64) {
+                    if (isLayoutRtl()){
+                        // The right side of the item is the grabber for dragging the item
+                        if (x > item.getRight() - 64){
+                            item.setDrawingCacheEnabled(true);
+                            // Create a copy of the drawing cache so that it does not get recycled
+                            // by the framework when the list tries to clean up memory
+                            Bitmap bitmap = Bitmap.createBitmap(item.getDrawingCache());
+                            startDragging(bitmap, x, y);
+                            mDragPos = itemnum;
+                            mSrcDragPos = mDragPos;
+                            mHeight = getHeight();
+                            int touchSlop = mTouchSlop;
+                            mUpperBound = Math.min(y - touchSlop, mHeight / 3);
+                            mLowerBound = Math.max(y + touchSlop, mHeight * 2 /3);
+                            return false;
+                        }
+                    }
+                    else if (x < 64) {
+                        // The left side of the item is the grabber for dragging the item
                         item.setDrawingCacheEnabled(true);
                         // Create a copy of the drawing cache so that it does not get recycled
                         // by the framework when the list tries to clean up memory
