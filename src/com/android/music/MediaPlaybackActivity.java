CodeLinaro-mirror/la_platform_packages_apps_SnapdragonMusic

Music: Support dual SIM card ringtone setting
Add function of setting music as Phone1 and Phone2 incoming call
ringtone.

Change-Id: Id44b69a99a756d1ac9e30cf70cdaf187a09502a7
CRs-Fixed: 619700

src/com/android/music/MediaPlaybackActivity.java
@@ -65,6 +65,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.view.KeyEvent;
+import android.telephony.MSimTelephonyManager;
 
 
 public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
@@ -601,8 +602,18 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     R.string.add_to_playlist).setIcon(android.R.drawable.ic_menu_add);
             // these next two are in a separate group, so they can be shown/hidden as needed
             // based on the keyguard state
-            menu.add(1, USE_AS_RINGTONE, 0, R.string.ringtone_menu_short)
-                    .setIcon(R.drawable.ic_menu_set_as_ringtone);
+            if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+                int[] ringtones = { USE_AS_RINGTONE, USE_AS_RINGTONE_2 };
+                int[] menuStrings = { R.string.ringtone_menu_short_1, R.string.ringtone_menu_short_2 };
+                for (int i = 0; i < MSimTelephonyManager.getDefault().getPhoneCount(); i++) {
+                    menu.add(0, ringtones[i], 0, menuStrings[i])
+                            .setIcon(R.drawable.ic_menu_set_as_ringtone);
+                }
+            } else {
+                menu.add(1, USE_AS_RINGTONE, 0, R.string.ringtone_menu_short)
+                        .setIcon(R.drawable.ic_menu_set_as_ringtone);
+            }
+
             menu.add(1, DELETE_ITEM, 0, R.string.delete_item)
                     .setIcon(R.drawable.ic_menu_delete);
 
@@ -659,6 +670,13 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     // Set the system setting to make this the current ringtone
                     if (mService != null) {
                         MusicUtils.setRingtone(this, mService.getAudioId());
+                    }
+                    return true;
+                }
+                case USE_AS_RINGTONE_2: {
+                    // Set the system setting to make this the current ringtone for SUB_1
+                    if (mService != null) {
+                        MusicUtils.setRingtone(this, mService.getAudioId(), MusicUtils.RINGTONE_SUB_1);
                     }
                     return true;
                 }

src/com/android/music/TrackBrowserActivity.java
@@ -62,6 +62,7 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.view.KeyEvent;
+import android.telephony.MSimTelephonyManager;
 
 import java.text.Collator;
 import java.util.Arrays;
@@ -698,7 +699,18 @@ public class TrackBrowserActivity extends ListActivity
         if (mEditMode) {
             menu.add(0, REMOVE, 0, R.string.remove_from_playlist);
         }
-        menu.add(0, USE_AS_RINGTONE, 0, R.string.ringtone_menu);
+
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+            int[] ringtones = { USE_AS_RINGTONE, USE_AS_RINGTONE_2 };
+            int[] menuStrings = { R.string.ringtone_menu_1,
+                                  R.string.ringtone_menu_2 };
+            for (int i = 0; i < MSimTelephonyManager.getDefault().getPhoneCount(); i++) {
+                menu.add(0, ringtones[i], 0, menuStrings[i]);
+            }
+        } else {
+            menu.add(0, USE_AS_RINGTONE, 0, R.string.ringtone_menu);
+        }
+
         menu.add(0, DELETE_ITEM, 0, R.string.delete_item);
         AdapterContextMenuInfo mi = (AdapterContextMenuInfo) menuInfoIn;
         mSelectedPosition =  mi.position;
@@ -759,6 +771,11 @@ public class TrackBrowserActivity extends ListActivity
             case USE_AS_RINGTONE:
                 // Set the system setting to make this the current ringtone
                 MusicUtils.setRingtone(this, mSelectedId);
+                return true;
+
+            case USE_AS_RINGTONE_2:
+                // Set the system setting to make this the current ringtone for SUB_1
+                MusicUtils.setRingtone(this, mSelectedId, MusicUtils.RINGTONE_SUB_1);
                 return true;
 
             case DELETE_ITEM: {

Citrus-CAF/packages_apps_SnapdragonMusic

src/com/android/music/MediaPlaybackActivity.java
@@ -46,6 +46,7 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.provider.MediaStore;
+import android.telephony.TelephonyManager;
 import android.text.Layout;
 import android.text.TextUtils.TruncateAt;
 import android.util.Log;
@@ -64,7 +65,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.view.KeyEvent;
 import com.android.music.SysApplication;
 
 
@@ -605,8 +605,18 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     R.string.add_to_playlist).setIcon(android.R.drawable.ic_menu_add);
             // these next two are in a separate group, so they can be shown/hidden as needed
             // based on the keyguard state
-            menu.add(1, USE_AS_RINGTONE, 0, R.string.ringtone_menu_short)
-                    .setIcon(R.drawable.ic_menu_set_as_ringtone);
+            if (TelephonyManager.getDefault().isMultiSimEnabled()) {
+                int[] ringtones = { USE_AS_RINGTONE, USE_AS_RINGTONE_2 };
+                int[] menuStrings = { R.string.ringtone_menu_short_1, R.string.ringtone_menu_short_2 };
+                for (int i = 0; i < TelephonyManager.getDefault().getPhoneCount(); i++) {
+                    menu.add(0, ringtones[i], 0, menuStrings[i])
+                            .setIcon(R.drawable.ic_menu_set_as_ringtone);
+                }
+            } else {
+                menu.add(1, USE_AS_RINGTONE, 0, R.string.ringtone_menu_short)
+                        .setIcon(R.drawable.ic_menu_set_as_ringtone);
+            }
+
             menu.add(1, DELETE_ITEM, 0, R.string.delete_item)
                     .setIcon(R.drawable.ic_menu_delete);
 
@@ -665,6 +675,13 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     // Set the system setting to make this the current ringtone
                     if (mService != null) {
                         MusicUtils.setRingtone(this, mService.getAudioId());
+                    }
+                    return true;
+                }
+                case USE_AS_RINGTONE_2: {
+                    // Set the system setting to make this the current ringtone for SUB_1
+                    if (mService != null) {
+                        MusicUtils.setRingtone(this, mService.getAudioId(), MusicUtils.RINGTONE_SUB_1);
                     }
                     return true;
                 }

src/com/android/music/TrackBrowserActivity.java
@@ -43,6 +43,7 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Audio.Playlists;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -711,7 +712,18 @@ public class TrackBrowserActivity extends ListActivity
         if (mEditMode) {
             menu.add(0, REMOVE, 0, R.string.remove_from_playlist);
         }
-        menu.add(0, USE_AS_RINGTONE, 0, R.string.ringtone_menu);
+
+        if (TelephonyManager.getDefault().isMultiSimEnabled()) {
+            int[] ringtones = { USE_AS_RINGTONE, USE_AS_RINGTONE_2 };
+            int[] menuStrings = { R.string.ringtone_menu_1,
+                                  R.string.ringtone_menu_2 };
+            for (int i = 0; i < TelephonyManager.getDefault().getPhoneCount(); i++) {
+                menu.add(0, ringtones[i], 0, menuStrings[i]);
+            }
+        } else {
+            menu.add(0, USE_AS_RINGTONE, 0, R.string.ringtone_menu);
+        }
+
         menu.add(0, DELETE_ITEM, 0, R.string.delete_item);
         AdapterContextMenuInfo mi = (AdapterContextMenuInfo) menuInfoIn;
         mSelectedPosition =  mi.position;
@@ -772,6 +784,11 @@ public class TrackBrowserActivity extends ListActivity
             case USE_AS_RINGTONE:
                 // Set the system setting to make this the current ringtone
                 MusicUtils.setRingtone(this, mSelectedId);
+                return true;
+
+            case USE_AS_RINGTONE_2:
+                // Set the system setting to make this the current ringtone for SUB_1
+                MusicUtils.setRingtone(this, mSelectedId, MusicUtils.RINGTONE_SUB_1);
                 return true;
 
             case DELETE_ITEM: {