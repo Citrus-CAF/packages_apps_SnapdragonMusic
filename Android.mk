730ac9fa78e5cc0e523ada127c1f4f2ab1fbd993
kaiyiz <kaiyiz@codeaurora.org>
2014-11-14 14:31:01 +0800

    Music: Make permission for sd card be effective.
    The permission is not effective which make delete file on sd
    card ineffective and cause this bug happen.
    
    Add certifaction to make the permission for sd card effective.
    
    CRs-Fixed: 754706
    
    Change-Id: Iccbde4c19ff2467bb64af4192ea29a441ac5b0fa

--- Android.mk
+++ Android.mk
@@ -4,9 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-	src/com/android/music/IMediaPlaybackService.aidl
+    src/com/android/music/IMediaPlaybackService.aidl
 
 LOCAL_PACKAGE_NAME := Music
+
+LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
