cfb2fb0111332d06963b381ec26fdf13fa4e6e43
yongga <yongga@codeaurora.org>
2016-01-14 19:41:42 +0800

    Music: set correctly mStatusListener register status to avoid crash.
    ensure mStatusListener register status is correct.
    
    Change-Id: I0a55795f5a72a771065e0177764ffcb37cca60ed
    CRs-Fixed: 957825

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -2071,7 +2071,6 @@ public class MediaPlaybackService extends Service {
                 Log.d(LOGTAG, "No play queue");
                 return;
             }
-
             int pos = getNextPosition(force);
             if (pos < 0) {
                 gotoIdleState();
@@ -2930,7 +2929,12 @@ public class MediaPlaybackService extends Service {
                             && mCurrentMediaPlayer != null
                             && mIsInitialized) {
                             mCurrentMediaPlayer.setNextMediaPlayer(mp);
+                            if (mNextMediaPlayer != null) {
+                                mNextMediaPlayer.release();
+                            }
                             mNextMediaPlayer = mp;
+                        } else {
+                            mp.release();
                         }
                     }
                 }, 300);
@@ -3015,6 +3019,7 @@ public class MediaPlaybackService extends Service {
                     mCurrentMediaPlayer.release();
                     if (mNextMediaPlayer != null) {
                         mNextMediaPlayer.release();
+                        mNextMediaPlayer = null;
                     }
                     // Creating a new MediaPlayer and settings its wakemode does not
                     // require the media service, so it's OK to do this now, while the
